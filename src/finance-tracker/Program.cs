cbaad25e1d4abb9cb8aeab4cda9fd3af2d29d114
William <williamcahill78@gmail.com>
2023-12-12 16:39:44 -0600

    Seperated user service functions into it's own static class

--- src/finance-tracker/Program.cs
+++ src/finance-tracker/Program.cs
@@ -42,23 +42,23 @@ class Program {
                 switch (choice)
                 {
                     case "1":
-                        Register(credentials, dbContext);
+                        UserService.Register(credentials, dbContext);
                         Console.ReadKey();
 
                         break;
                     case "2":
-                        TryLogin(credentials, dbContext);
+                        UserService.TryLogin(credentials, dbContext);
                         Console.ReadKey();
 
                         break;
                     case "3":
-                        ClearDatabase(dbContext);
+                        UserService.ClearDatabase(dbContext);
                         Console.Write("Database has been wiped.");
                         Console.ReadKey();
 
                         break;
                     case "4":
-                        PrintDatabaseContents(dbContext);
+                        UserService.PrintDatabaseContents(dbContext);
                         Console.ReadKey();
 
                         break;
@@ -72,75 +72,5 @@ class Program {
                 }
             }
         }
-    }
-    static (string name, string pass) RequestCredentials() {
-        Console.Write("Enter Username: ");
-        var newUsername = Console.ReadLine();
-
-        Console.Write("Enter Password: ");
-        var newPassword = Console.ReadLine();
-
-        return (newUsername, newPassword);
-    }
-
-    static void Register(Credentials creds, AppDbContext dbContext) {
-        var login = RequestCredentials();
-
-        var existingUser = dbContext.Users.FirstOrDefault(u => u.Username == login.name);
-
-        if (existingUser != null)
-        {
-            Console.WriteLine("User already exists. Registration failed.");
-            Console.ReadKey();
-        }
-        else
-        {
-            var newUser = new User { Username = login.name };
-            string hashedPassword = Credentials.Hash(login.pass);
-            newUser.Password = hashedPassword;
-
-            dbContext.Users.Add(newUser);
-
-            dbContext.SaveChanges();
-
-            creds.Register(login.name, login.pass);
-
-            Console.WriteLine("Registration Complete.");
-            Console.ReadKey();
-        }
-    }
-
-
-    static User TryLogin(Credentials creds, AppDbContext dbContext) {
-        var login = RequestCredentials();
-
-        // Fetch the user from the database
-        var user = dbContext.Users.FirstOrDefault(u => u.Username == login.name);
-
-        if (user != null)
-        {
-            if (creds.TryAuthenticate(login.name, login.pass))
-            {
-                Console.WriteLine("Login Successful");
-                return new User(login.name);
-            }
-        }
-
-        Console.WriteLine("Invalid username or password.");
-        return null;
-    }
-
-    static void PrintDatabaseContents(AppDbContext dbContext)
-    {
-        Console.WriteLine("Users in the Database:");
-        foreach (var user in dbContext.Users)
-        {
-            Console.WriteLine($"Username: {user.Username}, Password: {user.Password}");
-        }
-        Console.WriteLine();
-    }
-    public static void ClearDatabase(AppDbContext dbContext) {
-        dbContext.Database.EnsureDeleted();
-        dbContext.Database.EnsureCreated();
     }
 }
--- src/finance-tracker/Services/UserService.cs
+++ src/finance-tracker/Services/UserService.cs
@@ -4,4 +4,75 @@ public class UserService {
     public UserService(AppDbContext dbContext) {
         _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
     }
+
+    public static (string name, string pass) RequestCredentials() {
+        Console.Write("Enter Username: ");
+        var newUsername = Console.ReadLine();
+
+        Console.Write("Enter Password: ");
+        var newPassword = Console.ReadLine();
+
+        return (newUsername, newPassword);
+    }
+
+    public static void Register(Credentials creds, AppDbContext dbContext) {
+        var login = RequestCredentials();
+
+        var existingUser = dbContext.Users.FirstOrDefault(u => u.Username == login.name);
+
+        if (existingUser != null)
+        {
+            Console.WriteLine("User already exists. Registration failed.");
+            Console.ReadKey();
+        }
+        else
+        {
+            var newUser = new User { Username = login.name };
+            string hashedPassword = Credentials.Hash(login.pass);
+            newUser.Password = hashedPassword;
+
+            dbContext.Users.Add(newUser);
+
+            dbContext.SaveChanges();
+
+            creds.Register(login.name, login.pass);
+
+            Console.WriteLine("Registration Complete.");
+            Console.ReadKey();
+        }
+    }
+
+
+    public static User TryLogin(Credentials creds, AppDbContext dbContext) {
+        var login = RequestCredentials();
+
+        // Fetch the user from the database
+        var user = dbContext.Users.FirstOrDefault(u => u.Username == login.name);
+
+        if (user != null)
+        {
+            if (creds.TryAuthenticate(login.name, login.pass))
+            {
+                Console.WriteLine("Login Successful");
+                return new User(login.name);
+            }
+        }
+
+        Console.WriteLine("Invalid username or password.");
+        return null;
+    }
+
+    public static void PrintDatabaseContents(AppDbContext dbContext)
+    {
+        Console.WriteLine("Users in the Database:");
+        foreach (var user in dbContext.Users)
+        {
+            Console.WriteLine($"Username: {user.Username}, Password: {user.Password}");
+        }
+        Console.WriteLine();
+    }
+    public static void ClearDatabase(AppDbContext dbContext) {
+        dbContext.Database.EnsureDeleted();
+        dbContext.Database.EnsureCreated();
+    }
 }
